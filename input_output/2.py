63cfa66de4663a049b680d1f4c8f5cc126195233
JaSoul <young10270@gmail.com>
2017-01-18 15:42:38 +0900

    exercise input and out

---
+++ input_output/2.py
@@ -0,0 +1,9 @@
+in_str=input("ID를 입력해주세요 : ")
+real_jsoul="11"
+real_julie="ab"
+if real_jsoul==in_str:
+    print("Hello JSoul")
+elif real_julie==in_str:
+    print("Hello Julie")
+else:
+    print("Who are you?")
---
+++ input_output/2.rb
@@ -0,0 +1,11 @@
+puts("ID를 입력해주세요 : ")
+in_str=gets.chomp()
+real_jsoul="11"
+real_julie="ab"
+if real_jsoul==in_str
+  puts("Hello JSoul")
+elsif real_julie==in_str
+  puts("Hello Julie")
+else
+  puts("Who are you?")
+end
