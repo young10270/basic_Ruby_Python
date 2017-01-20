1a26ac60a7e8dd3dac8b3236bf29ac17d342d22f
JaSoul <young10270@gmail.com>
2017-01-20 17:47:17 +0900

    exercise function

---
+++ function/3.rb
@@ -0,0 +1,15 @@
+5.times() {|i| puts i}
+2.times() {puts '2times'}
+3.upto(5) {|item| puts item}
+
+
+arr = ['A','B','C']
+arr.each() {|i| puts i}
+
+arr = [1,3,56,7,13,52]
+arr.delete_if() do |item|
+  item>7
+end
+puts arr
+
+#study blcok
