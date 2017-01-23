dd227f0a467e23636d92063fae8201865d8fdaed
JaSoul <young10270@gmail.com>
2017-01-23 14:00:25 +0900

    exercise concept of OOP

---
+++ OOP/1.rb
@@ -0,0 +1,11 @@
+name1 = String.new 'julie'
+name2 = String.new 'jsoul'
+puts name1.reverse
+puts name2.reverse
+puts name1.upcase
+puts name1.size
+names = Array.new
+names.push 'julie'
+names.push 'jsoul'
+puts names
+puts names.join ','
